d1ea7a57b688ff1d9e153583dccca000e6e6e3c4
Abhishek Chandratre <abhishek.chandratre@gmail.com>
2017-12-22 00:38:33 -0500

    Added program arguments -d domain -n notebook -p page

--- notes/core.py
+++ notes/core.py
@@ -1,4 +1,19 @@
 from notes.params import *
+import argparse
+
+def parse_and_return_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--domain", help="domain of notes;" +
+            " if domain is empty then print all domains")
+    parser.add_argument("-n", "--notebook", help="notebook within domain;" +
+            " if notebook is empty then show all notebooks of given domain")
+    parser.add_argument("-p", "--page", help="page within notebook;" +
+            " if page is empty then show all pages of given notebook")
+    args = parser.parse_args()
+    domain = args.domain; notebook = args.notebook; page = args.page
+    return (domain, notebook, page)
+
 
 def handle_main():
-    print (NOTES_PATH)
+    print ("notes path:", NOTES_PATH)
+    (domain_name, notebook_name, page_name) = parse_and_return_arguments()
